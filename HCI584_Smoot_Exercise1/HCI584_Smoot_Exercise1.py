cfe3b38a13f8dcc8251d497066dc8328997378bf
jsmoot <jsmoot3@hotmail.com>
2020-05-27 13:47:27 -0700

    Add project files.

---
+++ HCI584_Smoot_Exercise1/HCI584_Smoot_Exercise1.py
@@ -0,0 +1,8 @@
+#Assigment one for HCI 584
+#by James Smoot
+
+print("start of part 0")
+print ("This is the first assigment for this class")
+name = input("What is your Name ")
+print("What's Up", name)
+print("end of 0")
