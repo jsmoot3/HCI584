c145c5779d907977c032ff35b6f0f2760d477be3
James <jsmoot3@hotmail.com>
2020-05-30 09:25:27 -0700

    start

--- HCI584_Smoot_Exercise1/HCI584_Smoot_Exercise1.py
+++ HCI584_Smoot_Exercise1/HCI584_Smoot_Exercise1.py
@@ -1,8 +1,104 @@
 #Assigment one for HCI 584
 #by James Smoot
 
+# part 0
+# This is just a simple example to demonstrate how the start and end
+# breakpoint system works
+# Task: create a variable with your name and print out Hello <name>
+
 print("start of part 0")
 print ("This is the first assigment for this class")
 name = input("What is your Name ")
 print("What's Up", name)
 print("end of 0")
+
+print("****************************************************************************")
+# part 1
+# task:  L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
+# using both, indexing and slicing on L, assemble and print a new list N that contains:
+# [0,2,3,[5,6],8,10] 
+# as an example, here's  how i've constructed a similar list X which contains [[2, 3], [10]] from L:
+#
+# L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
+# X = [ L[2][1:-1], L[-1][2] ] 
+# print(x)  => [[2, 3], [10]]
+# You need to do something similar but end up with [0,2,3,[5,6],8,10] instead. One way to work 
+# through this is to break the process down in small steps, store result of each step in a new variable
+# and use those variables in the next step
+print("start of part 1") # set breakpoint here
+L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
+print(L)
+# your code
+
+
+print("end of 1") # set breakpoint here 
+print("****************************************************************************")
+
+# solution 2
+L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
+print("L is", L)
+tmp1 = L[0] # 0
+tmp2 = L[2][1] # 2
+tmp3 = L[2][2] # 3
+tmp4 = [L[3][0][0], L[3][1][0]] # [5, 6]
+tmp5 = L[4][0] # 8
+tmp6 = L[4][-1] # 10 
+newL = [tmp1, tmp2, tmp3, tmp4, tmp5, tmp6] # create final list
+print(newL) # [0, 2, 3, [5, 6], 8, 10]
+
+
+# part 2
+# Task: Break s into sentences (which end in a .), count them and print them out using a loop:
+# result: (I truncated them with ...)
+# there are 4 sentences:
+# Python is an interpreted, high-level, general-purpose programming language
+#  Created by Guido van Rossum and first released in 1991, Python ...
+#  Its language constructs and object-oriented approach aim to help programmers ...
+print("start of part 2") # set breakpoint here
+s = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
+# your code here
+
+print("end of 2") # set breakpoint here 
+print("****************************************************************************")
+
+# part 3
+# Task: 
+# - break s into a list of words (i.e. now separated by space)
+# - print out the word list (with a loop) so that every 2. word is in full uppercase.
+# - optionally remove all periods and commas
+# result:
+# Python
+# IS
+# an
+# INTERPRETED
+# high-level
+# GENERAL-PURPOSE
+# programming
+# LANGUAGE
+print("start of part 3") # set breakpoint here
+# your code here
+
+
+print("end of 3") # set breakpoint here 
+print("****************************************************************************")
+
+# part 4
+# task: abbreviate a potentially long string s to have the x first and x last chars with ... in between
+# for x = 5 this would be: "A very long description" => "A ver...ption" (... is called filler)
+# in a loop, set x from 5 to and to 15 and print out x and the abbreviated version 
+# there'll be an issue where the result would actually be longer(!) than the un-abbreviated s. 
+# For these cases, do not perform your abbreviation, simply print out s. Note that this
+# should work for any other string a or filler as well, so don't hardcode things!
+# 
+# Optional:
+# write a general function abbr(s, filler="...", total_width=15) which abbreviates s
+# to total_width chars and uses the string filler in between them. Again, make sure the
+# result would not be longer than s!
+# call your function a couple of times with different parameters and also test edge cases
+print("start of part 4") # set breakpoint here
+s = "A very long description" # a long string
+filler = "..."
+# your code here
+
+
+print("end of 4") # set breakpoint here 
